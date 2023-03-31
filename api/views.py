c35996a6ca03d628eab144a8bfc8f383e5d554cb
mac-rifat <macrifat@gmail.com>
2023-03-31 02:46:04 +0200

    CRUD:post,put,delete

--- api/views.py
+++ api/views.py
@@ -18,8 +18,43 @@ def apiOverview(request):
 		'Delete':'/task-delete/<str:pk>/'
 		}
     return Response(api_urls)
+
 @api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all()
     serializer= TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer= TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer= TaskSerializer(data=request.data)
+    print('serializer#####',serializer)
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    tasks = Task.objects.get(id=pk)
+    print('Taskssssssssssssss######', tasks)
+    serializer= TaskSerializer(instance=tasks,data=request.data)
+    print('serializer#####',serializer)
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    tasks = Task.objects.get(id=pk)
+    tasks.delete()
+    
+    return Response('SUCCESSFULLY DELETED')
